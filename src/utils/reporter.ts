83730153e00558ccd159678dd5a3b9b0f6592e5b
Jean Dessane <jean@habx.fr>
2020-01-29 11:39:47 +0100

    fix reporting

--- src/utils/reporter.ts
+++ src/utils/reporter.ts
@@ -39,60 +39,62 @@ export default async (globals: NightwatchGlobals, results) => {
       }
     })
     const testsCount = Number(get(moduleReport, 'assertionsCount', 0))
-    generalReport[moduleReportName] = {
-      sessionid: globals.sessionid,
-      raw: results,
-      browserstackLinks: {
-        ...get(globals.browserstackLinks, moduleReportName, {}),
-        ...get(generalReport, [moduleReportName, 'browserstackLinks'], {}),
-      },
-      lastUrl:
-        globals.lastUrl || get(generalReport, [moduleReportName, 'lastUrl']),
-      failedImageUrl:
-        globals.failedImageUrl ||
-        get(generalReport, [moduleReportName, 'failedImageUrl']),
-      failedTests: {
-        ...get(generalReport, [moduleReportName, 'failedTests'], {}),
-        ...failedTests,
-      }, // TODO: add errors
-      envs: uniq([
-        ...get(generalReport, [moduleReportName, 'envs'], []),
-        globals.env,
-      ]),
-      devices: uniqBy(
-        [
-          ...get(generalReport, [moduleReportName, 'devices'], []),
-          {
-            label: globals.deviceName,
-            uiRegression: failedScreenshotCompareCount > 0,
-            failed: Object.values(failedTests).length > 0,
-          },
-        ],
-        'label'
-      ),
-      totalTime:
-        Number(get(generalReport, [moduleReportName, 'totalTime'], 0)) +
-        Number(get(moduleReport, 'time', 0)),
-      testsCount,
-      failedCount:
-        Number(get(generalReport, [moduleReportName, 'failedCount'], 0)) +
-        Number(get(moduleReport, 'failedCount', 0)),
-      errorsCount:
-        Number(get(generalReport, [moduleReportName, 'errorsCount'], 0)) +
-        Number(get(moduleReport, 'errorsCount', 0)),
-      passedCount:
-        Number(get(generalReport, [moduleReportName, 'passedCount'], 0)) +
-        Number(get(moduleReport, 'passedCount', 0)),
-      firstScreenshotFailed,
-      failedScreenshotCompareCount:
-        failedScreenshotCompareCount +
-        Number(
-          get(
-            generalReport,
-            [moduleReportName, 'failedScreenshotCompareCount'],
-            0
-          )
+    if (testsCount > 0) {
+      generalReport[moduleReportName] = {
+        sessionid: globals.sessionid,
+        raw: results,
+        browserstackLinks: {
+          ...get(globals.browserstackLinks, moduleReportName, {}),
+          ...get(generalReport, [moduleReportName, 'browserstackLinks'], {}),
+        },
+        lastUrl:
+          globals.lastUrl || get(generalReport, [moduleReportName, 'lastUrl']),
+        failedImageUrl:
+          globals.failedImageUrl ||
+          get(generalReport, [moduleReportName, 'failedImageUrl']),
+        failedTests: {
+          ...get(generalReport, [moduleReportName, 'failedTests'], {}),
+          ...failedTests,
+        }, // TODO: add errors
+        envs: uniq([
+          ...get(generalReport, [moduleReportName, 'envs'], []),
+          globals.env,
+        ]),
+        devices: uniqBy(
+          [
+            ...get(generalReport, [moduleReportName, 'devices'], []),
+            {
+              label: globals.deviceName,
+              uiRegression: failedScreenshotCompareCount > 0,
+              failed: Object.values(failedTests).length > 0,
+            },
+          ],
+          'label'
         ),
+        totalTime:
+          Number(get(generalReport, [moduleReportName, 'totalTime'], 0)) +
+          Number(get(moduleReport, 'time', 0)),
+        testsCount,
+        failedCount:
+          Number(get(generalReport, [moduleReportName, 'failedCount'], 0)) +
+          Number(get(moduleReport, 'failedCount', 0)),
+        errorsCount:
+          Number(get(generalReport, [moduleReportName, 'errorsCount'], 0)) +
+          Number(get(moduleReport, 'errorsCount', 0)),
+        passedCount:
+          Number(get(generalReport, [moduleReportName, 'passedCount'], 0)) +
+          Number(get(moduleReport, 'passedCount', 0)),
+        firstScreenshotFailed,
+        failedScreenshotCompareCount:
+          failedScreenshotCompareCount +
+          Number(
+            get(
+              generalReport,
+              [moduleReportName, 'failedScreenshotCompareCount'],
+              0
+            )
+          ),
+      }
     }
   }
   writeReportData(generalReport)
